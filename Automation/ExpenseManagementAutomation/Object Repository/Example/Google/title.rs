bcb973db0f7bd795a9ea87a49243143e7cdc366f
Chanakya Dandamudi <dandamudichanakya@gmail.com>
2023-10-06 15:32:43 -0500

    EMS-21 Added example test case

---
+++ Automation/ExpenseManagementAutomation/Object Repository/Example/Google/title.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>title</name>
+   <tag></tag>
+   <elementGuidId>290d0647-4fea-42f8-a376-d0b2f1f01c79</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[1]/div[2]/div/img</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
