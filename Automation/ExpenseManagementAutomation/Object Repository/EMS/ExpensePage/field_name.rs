76f969942f46abda445e877fc6cb1799f9bb9284
Chanakya Dandamudi <dandamudichanakya@gmail.com>
2023-10-11 23:33:27 -0500

    EMS-20_Added Automation template for story

---
+++ Automation/ExpenseManagementAutomation/Object Repository/EMS/ExpensePage/field_name.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>field_name</name>
+   <tag></tag>
+   <elementGuidId>91fb4b65-3715-4e64-8fd8-e570a787dc3d</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Automation/ExpenseManagementAutomation/Object Repository/EMS/HomePage/label_month.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label_month</name>
+   <tag></tag>
+   <elementGuidId>d3579ab1-5eed-4418-ac36-1cbea242b7b2</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
