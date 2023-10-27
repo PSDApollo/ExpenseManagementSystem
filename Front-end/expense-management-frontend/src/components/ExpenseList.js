b818178a414a90b56859c11b4d56309fc4657e14
vnandala <vnandala@slu.edu>
2023-10-27 15:45:40 -0500

    EMS-47 Added changes to Expense List Page

--- Front-end/expense-management-frontend/src/components/ExpenseList.js
+++ Front-end/expense-management-frontend/src/components/ExpenseList.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import '../expenseliststyle.css'; // Import your CSS file for styling
  
 function ExpenseList() {
   const [expenses, setExpenses] = useState([]);
  
   useEffect(() => {
     console.log('Fetching expenses...');
-    fetch('https://15af-2600-6c40-75f0-ffc0-dc90-95b4-5282-a6e0.ngrok-free.app/expenses', {
-      origin: 'http://localhost:3000/',
-    headers: {
-        'Authorization': localStorage.getItem('userKey') // Send the user key in headers
-    }
-})
+    const key = localStorage.getItem('myKey'); // Get the key from local storage
+ 
+    if (key) {
+      fetch('https://15af-2600-6c40-75f0-ffc0-dc90-95b4-5282-a6e0.ngrok-free.app/expenses', {
+        method: 'GET',
+        headers: {
+          'Authorization': key, // Use the key as the Authorization header
+        }
+      })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the response as JSON
+      })
       .then((data) => {
         console.log('Data received:', data);
         setExpenses(data);
@@ -18,21 +28,36 @@ function ExpenseList() {
       .catch((error) => {
         console.error('Error fetching expenses:', error);
       });
+    } else {
+      console.error('Key not found in local storage');
+    }
   }, []);
  
   return (
     <div>
       <h1>Expense List</h1>
-      <ul>
-        {expenses.map((expense) => (
-          <li key={expense.id}>
-            {expense.name} - {expense.amount}
-          </li>
-        ))}
-      </ul>
+      <table className="expense-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Expense Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.name}</td>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{new Date(expense.expense_date).toLocaleDateString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
  
 export default ExpenseList;
- 
