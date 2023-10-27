9f3463ce69005deb05779ca2d52e96d5c3c0835e
sandeepreddiee <78849009+sandeepreddiee@users.noreply.github.com>
2023-10-27 16:42:02 -0500

    Revert "Complete End-to-End Model"

--- Front-end/expense-management-frontend/src/components/Login.js
+++ Front-end/expense-management-frontend/src/components/Login.js
@@ -5,82 +5,75 @@ import '../loginstyle.css';
 import expenseManagementImage from '../images/naassom-azevedo-Q_Sei-TqSlc-unsplash.jpg';
 
 function Login() {
-  const navigate = useNavigate();
-  const [credentials, setCredentials] = useState({
-    email: '',
-    password: ''
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setCredentials({
-      ...credentials,
-      [name]: value
+    const navigate = useNavigate();
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
     });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();  // Prevent the form from reloading the page.
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({
+            ...credentials,
+            [name]: value
+        });
+    };
 
-    fetch('https://15af-2600-6c40-75f0-ffc0-dc90-95b4-5282-a6e0.ngrok-free.app/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(credentials),
-    })
-    .then((response) => {
-      if (response.ok) {
-        console.log('Login Successful!');
-        window.alert('Login Success');
-        return response.text(); // Get the response text
-      } else {
-        window.alert('Login failed');
-        throw new Error('Login failed');
-      }
-    })
-    .then((key) => {
-      localStorage.setItem('myKey', key); // Store the key in local storage
-      console.log('Key received: ', key);
-      navigate('/dashboard');
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        fetch('https://15af-2600-6c40-75f0-ffc0-dc90-95b4-5282-a6e0.ngrok-free.app/login', {
+            method: 'POST',
+            origin: 'http://localhost:3000/',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.success) {
+                navigate('/dashboard');
+            } else {
+                alert('Login failed!');
+            }
+        })
+        .catch(error => {
+            console.error('Login Error:', error);
+        });
+    };
 
-  return (
-    <div className="container">
-      <div className="image-section">
-        <img src={expenseManagementImage} alt="Expense Management" />
-        <div className="tagline">Plan. Track. Thrive.<br />Expense Management the Apollo Way.</div>
-      </div>
-      <div className="form-section">
-        <h1>Hello Apollo!</h1>
-        <p>Welcome back you've been missed!</p>
-        <form onSubmit={handleSubmit}>
-          <input 
-            type="text" 
-            className="username"
-            name="email" 
-            placeholder="Enter username" 
-            onChange={handleInputChange} 
-          />
-          <input 
-            type="password" 
-            className="password"
-            name="password" 
-            placeholder="Password" 
-            onChange={handleInputChange} 
-          />
-          <button type="submit">Sign In</button>
-          <div className="signup-text">
-            Don't have an account? <Link to="/signup">Signup</Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="image-section">
+                <img src={expenseManagementImage} alt="Expense Management" />
+                <div className="tagline">Plan. Track. Thrive.<br />Expense Management the Apollo Way.</div>
+            </div>
+            <div className="form-section">
+                <h1>Hello Apollo!</h1>
+                <p>Welcome back you've been missed!</p>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        class = "username"
+                        name="email" 
+                        placeholder="Enter username" 
+                        onChange={handleInputChange} 
+                    />
+                    <input 
+                        type="password" 
+                        class = "password"
+                        name="password" 
+                        placeholder="Password" 
+                        onChange={handleInputChange} 
+                    />
+                    <button type="submit">Sign In</button>
+                    <div className="signup-text">
+                    Don't have an account? <Link to="/signup">Signup</Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
