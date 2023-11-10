0ac670bc52a157651cb68e6534b5edb8c8522f2b
Chanakya Dandamudi <dandamudichanakya@gmail.com>
2023-11-10 14:10:40 -0600

    Dashboard test updated

--- Front-end/expense-management-frontend/src/tests-dashboard/Dashboard.test.js
+++ Front-end/expense-management-frontend/src/tests-dashboard/Dashboard.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
+import Dashboard from '../components/Dashboard';
 
 describe('Dashboard component', () => {
   it('renders header and buttons', () => {
