a355b436c6ad15d350566b6f1c8b937b81c4a9b3
sandeepreddiee <78849009+sandeepreddiee@users.noreply.github.com>
2023-11-10 11:20:41 -0600

    Dashboard & AddExpense Based on USER specific

--- Front-end/expense-management-frontend/src/components/AddExpense.js
+++ Front-end/expense-management-frontend/src/components/AddExpense.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../style.css';
+import '../addexpensestyle.css';
 
 function AddExpense() {
   const history = useNavigate();
