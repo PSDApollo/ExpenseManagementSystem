24239cc168f68ea5d7de935c0c3bc934346016ce
Chanakya Dandamudi <dandamudichanakya@gmail.com>
2023-11-30 16:48:20 -0600

    reverting

--- Front-end/expense-management-frontend/src/components/ExpenseList.js
+++ Front-end/expense-management-frontend/src/components/ExpenseList.js
@@ -1,64 +1,88 @@
 import React, { useEffect, useState } from 'react';
-import '../expenseliststyle.css'; 
 
 function ExpenseList() {
   const [expenses, setExpenses] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState('USD');
+  const [exchangeRates, setExchangeRates] = useState({
+    USD: { symbol: '$', rate: 1 },
+    EUR: { symbol: '€', rate: 0.85 },
+    INR: { symbol: '₹', rate: 73.5 },
+  });
 
   useEffect(() => {
-    // Fetching expenses from the backend
+    console.log('Fetching expenses...');
     const key = localStorage.getItem('myKey');
 
     if (key) {
-      fetch('https://<your-backend-url>/expenses', {
+      fetch('http://localhost:9111/expenses', {
         method: 'GET',
         headers: {
           'Authorization': key,
         },
       })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setExpenses(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching expenses:', error);
-      });
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json(); // Parse the response as JSON
+        })
+        .then((data) => {
+          console.log('Data received:', data);
+          setExpenses(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching expenses:', error);
+        });
     } else {
       console.error('Key not found in local storage');
     }
   }, []);
 
+  const handleCurrencyChange = (event) => {
+    const newCurrency = event.target.value;
+    setSelectedCurrency(newCurrency);
+  };
+
+  const convertAmountToSelectedCurrency = (amount, currency) => {
+    const rate = exchangeRates[currency].rate;
+    return (amount * rate).toFixed(2);
+  };
+
+  const getCurrencySymbol = (currency) => {
+    return exchangeRates[currency].symbol;
+  };
+
   return (
-    <div className="expense-list-container">
-      <h1 className="expense-list-title">Expense List</h1>
-      {expenses.length > 0 ? (
-        <table className="expense-list-table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Amount</th>
-              <th>Expense Date</th>
+    <div>
+      <h1>Expense List</h1>
+      <div style={{ float: 'right', marginBottom: '10px' }}>
+        <label htmlFor="currency">Currency:</label>
+        <select id="currency" value={selectedCurrency} onChange={handleCurrencyChange}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+      <table className="expense-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Expense Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.name}</td>
+              <td>{expense.description}</td>
+              <td>{getCurrencySymbol(selectedCurrency)}{convertAmountToSelectedCurrency(expense.amount, selectedCurrency)}</td>
+              <td>{new Date(expense.expense_date).toLocaleDateString()}</td>
             </tr>
-          </thead>
-          <tbody>
-            {expenses.map((expense) => (
-              <tr key={expense.id}>
-                <td>{expense.name}</td>
-                <td>{expense.description}</td>
-                <td>${expense.amount.toFixed(2)}</td>
-                <td>{new Date(expense.expense_date).toLocaleDateString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p className="expense-list-no-data">No expenses to display</p>
-      )}
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
